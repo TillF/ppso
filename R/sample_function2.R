f6da6b14a994142c98e70c11e1ea0d72ac33a4d2
francke <TillF@users.noreply.github.com>
2009-09-21 11:42:51 +0000

    - introduced DDS (Dynamically Dimensioned Search) - updated docu - added reknown smaple functions (Rastrigin, Ackley, Griewank) - added max_number_function_calls as parameter

--- R/sample_function2.R
+++ R/sample_function2.R
@@ -1,12 +1,11 @@
 sample_function2 <-
-function(param_vec,fast=FALSE)          #objective function to be minimized
+function(param_vec,maxwaittime=0)          #objective function to be minimized
 {
-  r <- sqrt((param_vec[1]*4)^2+(param_vec[2]*4)^2) 
+  r <- max(1e-320,sqrt((param_vec[1]*4)^2+(param_vec[2]*4)^2)) 
   obj=-5 * sin(r)/r 
 
-  if (!fast)
+  if (maxwaittime>0)  #function to be run on slaves, with random delay
   {
-    maxwaittime=0.1               #function to be run on slaves, with random delay
     starttime=Sys.time()
     waittime= runif(1,0,maxwaittime)
     while (as.numeric(Sys.time()-starttime)<waittime)

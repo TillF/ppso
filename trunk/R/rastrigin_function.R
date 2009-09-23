dd127370f262551ec1d5c78f4df49d2eb17043af
francke <francke@6deb71d0-20cc-4284-8dc2-2dc2e9dd78f8>
2009-09-23 11:01:10 +0000

    introduced parallel DDS fixed bug in DDS (overreflection) introduced max_number_function_calls fixed bug concerning existence of logfile revised docu

---
+++ trunk/R/rastrigin_function.R
@@ -0,0 +1,20 @@
+#test function after Rastrigin (1974), minimum value=-dimen
+rastrigin_function <-
+function(param_vec,maxwaittime=0)          #objective function to be minimized
+{
+  dimen=length(param_vec);
+
+  obj=sum(param_vec^2-cos(2*pi*param_vec))
+
+  if (maxwaittime>0)  #function to be run on slaves, with random delay
+  {
+    starttime=Sys.time()
+    waittime= runif(1,0,maxwaittime)
+    while (as.numeric(Sys.time()-starttime)<waittime)
+    {
+    }
+  }
+  return(obj)
+}
+
+

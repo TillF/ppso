5210559a06f119e0adf89925e9f6208efdcd2a7f
francke <francke@6deb71d0-20cc-4284-8dc2-2dc2e9dd78f8>
2016-05-04 07:39:19 +0000

    potentially relevant for p*: polling intervalls of master may have been set too long, resulting in delays or apparent hangs.

--- trunk/R/check_execution_timeout.R
+++ trunk/R/check_execution_timeout.R
@@ -8,14 +8,14 @@ check_execution_timeout = function()
   verbose_master = globvars$verbose_master #get global value
 
   nodes=unique(globvars$node_id[globvars$status==2])                         #check all nodes that are currently employed
-
+  min_exp_runtime = Inf #minimum expected runtime of all active slaves
+  
   for (i in nodes)
   {
     
     current_particle = globvars$node_id==i & (globvars$status==2)
 
     current_time_execution=difftime(Sys.time(),globvars$computation_start[current_particle],units="secs")       #get the time this node has been processing the current call so far
-    if (verbose_master) {print(paste0(Sys.time()," ...slave ", i,":", current_time_execution)); flush.console()}
     
     calls_so_far = globvars$execution_times$slave_id==i                                 #calls performed by this node so far
     if(sum(calls_so_far)==0)                                             #no completed calls so far
@@ -26,12 +26,15 @@ check_execution_timeout = function()
     else
       mean_execution_time = mean(globvars$execution_times$secs[calls_so_far])    #calculate the mean execution time of this node so far
 
+    exp_runtime = max(0, mean_execution_time - current_time_execution) #expected runtime for this slave
+    min_exp_runtime = min (exp_runtime, min_exp_runtime)       #minimum of all expected runtimes
+    
   
     if (current_time_execution > mean_execution_time*globvars$execution_timeout)     #current call takes too long?
     {
-       if (verbose_master) {print(paste0(Sys.time()," ...current execution time (", current_time_execution,") greater than benchmark (",mean_execution_time,")")); flush.console()}
-       if (verbose_master) print(paste(Sys.time()," ...slave",i,"exceeded timeout, resetting..."))
-              
+       if (verbose_master) 
+       {print(paste0(Sys.time()," ...current execution time (", current_time_execution,") greater than", globvars$execution_timeout, "* benchmark (",mean_execution_time,"), resetting...")); flush.console()}
+
        globvars$node_id[current_particle] = 0          #reset particle
        globvars$status [current_particle] = 0  
        globvars$slave_status[i,"counter"] = globvars$slave_status[i,"counter"] +1   #increase counter of total interruptions
@@ -40,17 +43,17 @@ check_execution_timeout = function()
        if (globvars$slave_status[i,"timeouts_in_row"] > maxtries)
        {
         globvars$closed_slaves = globvars$closed_slaves + 1
-        warning(paste("Permanently excluded slave",i,"because of failing to produce results within",mean_execution_time,"s for",maxtries,"attempts."))
+        warning(paste("Permanently excluded slave",i,"because of failing to produce results within",mean_execution_time*globvars$execution_timeout,"s for",maxtries,"attempts."))
        }                               
     } else
-    if (verbose_master) {print(paste0(Sys.time()," ...current execution time (", current_time_execution,") less than benchmark (",mean_execution_time,")")); flush.console()}
+    if (verbose_master) {print(paste0(Sys.time()," ...current execution time (", current_time_execution,") less than ", globvars$execution_timeout, "* benchmark (",mean_execution_time,")")); flush.console()}
     
   }
 
 
   if (length(globvars$execution_times$secs)==0)
-    return(0.1)
+    return(0.1) #no measured execution times so far
   else
-    return(min(c(1,globvars$execution_times$secs)))
+    return(min(30, min_exp_runtime)) #wait the minimum expected time until a slave finishes, but not more than 30 s
 #return(0)     #rr
 }
